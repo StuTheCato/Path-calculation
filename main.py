8e5a2db98080fa924668e5d026266994b040ec14
Stu <88785817+StuTheCoder@users.noreply.github.com>
2022-09-22 08:11:53 +0200

    main.py
    Note: This is just a practice code idea. This was my school homework xD

---
+++ main.py
@@ -0,0 +1,33 @@
+Speed = input("Type your speed (km/h): ")
+reactionTime = int(Speed) / 10 * 3
+breakTime = reactionTime * 2
+stopTime = reactionTime + breakTime
+
+print("1 - Reaction Time")
+print("2 - Break Time")
+print("3 - Stop Time")
+print("4 - All")
+print("")
+print("....................................")
+print("")
+question = input("What do you need to know?: ")
+print("")
+
+if (question == '1'):
+    print(reactionTime)
+elif (question == '2'):
+    print(breakTime)
+elif (question == '3'):
+    print(stopTime)
+elif (question == '4'):
+    print(f"Reaction Time: {reactionTime}")
+    print(f"Break Time: {breakTime}")
+    print(f"Stop Time: {stopTime}")
+    
+#    /\_____/\
+#   /  o   o  \
+#  ( ==  ^  == )
+#   )         (
+#  (           )
+# ( (  )   (  ) )
+# (__(__)___(__)__)
